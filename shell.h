69a5f1e79d8b3afe0207703b0cc7359ae8c57f58
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 <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>
2024-04-25 12:33:17 +0200

    shell_h

--- shell.h
+++ shell.h
@@ -6,4 +6,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <errno.h>
 
+#define BUFFER_SIZE 1024
+
+void display_prompt();
+char *read_line();
+char *read_line_file(FILE *file);
+void error(void);
+
+#endif /* SHELL_H*/
