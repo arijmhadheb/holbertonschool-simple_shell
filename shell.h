d7ae9cc047f2c9609b63242eff858b9375538447
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 <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>
2024-04-25 13:25:16 +0200

    shell_h

--- shell.h
+++ shell.h
@@ -9,10 +9,13 @@
 #include <errno.h>
 
 #define BUFFER_SIZE 1024
+#define EXIT_SUCCESS 0
 
 void display_prompt();
 char *read_line();
 char *read_line_file(FILE *file);
 void error(void);
+void pwd_command (void);
+int exit(void);
 
 #endif /* SHELL_H*/
